3d4dec1225a803ddac1e420e4c58027eebb6c64a
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-28 01:16:44 +0400

    update

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDHYZbnDXlhPpDy15tTCRVszs9XlYiDdu0",
+    authDomain: "netflix-clone-fcd36.firebaseapp.com",
+    projectId: "netflix-clone-fcd36",
+    storageBucket: "netflix-clone-fcd36.appspot.com",
+    messagingSenderId: "577839074259",
+    appId: "1:577839074259:web:c89200ff5513db0048bd26"
+  };
