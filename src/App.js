8945381e395b12792053abf51fc18c23e85efd10
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-28 14:49:30 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Row from './components/Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="app">
-      Netflix
+      <Row title="Netflix Original" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
