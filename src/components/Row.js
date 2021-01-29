c7a320804c8ab79d57fb8318651b6afe08bf3e6e
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-29 17:04:44 +0400

    finish

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,22 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import './Row.css';
 import axios from '../axios';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
 
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
-            //console.log(request)
             return request;
         }
         fetchData();
     }, [fetchUrl]);
 
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        }
+    };
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('');
+        } else {
+            movieTrailer(movie?.name || "")
+            .then( url => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+            }).catch(error => console.log('Please fix ' + error));
+        }
+    }
             
 
 
@@ -30,10 +52,12 @@ function Row({ title, fetchUrl, isLargeRow }) {
                         key={movie.id}
                         className="row__poster"
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                        alt={movie.name} />
-                    
+                        alt={movie.name} 
+                        onClick={ () => handleClick(movie)}
+                    />
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
